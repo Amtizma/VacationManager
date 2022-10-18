00d97ffe3b9a427ba132717f2a0d7f64d8bef832
Florin Denes <denesflorin37@gmail.com>
2022-10-18 09:25:54 +0300

    a

---
+++ src/main/java/com/example/VacationManager/controller/City.java
@@ -0,0 +1,19 @@
+package com.example.VacationManager.controller;
+
+public class City {
+    public City() {
+    }
+
+    public String city;
+
+    public String getCity() {
+        return city;
+    }
+
+
+    public void setCity(String City) {
+        city = City;
+    }
+
+
+}
